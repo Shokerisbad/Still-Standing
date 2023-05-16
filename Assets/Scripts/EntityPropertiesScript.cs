9aa386622ae34dc7c297938c2dc3d46d9979fb82
Marianmariam <razvanbanescu452@gmail.com>
2023-05-16 10:37:04 +0300

    health bar

--- Assets/Scripts/EntityPropertiesScript.cs
+++ Assets/Scripts/EntityPropertiesScript.cs
@@ -7,20 +7,32 @@ public class EntityPropertiesScript : MonoBehaviour
     public int damage = 0;
     public int maxHealth = 10;
     public int health = 0;
+    //public bool alive;
+    
 
     // Start is called before the first frame update
     void Start()
     {
        
         health = maxHealth;
+        //alive = true;
     }
+
+    public int GetHp()
+    {
+        return health;
+    }
+    
 
     public void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            //alive = false;
+            health = 0;
+            Destroy(gameObject, 0.1f);
+
         }
     }
 
@@ -32,4 +44,22 @@ public class EntityPropertiesScript : MonoBehaviour
         }
     }
 
+   /* public bool Alive()
+    {
+        return alive;
+    }
+
+      public void CheckIfAlive()
+    {
+        if (health > 0)
+        {
+            alive = true;
+        }
+        else if (health <= 0)
+        {
+            // imager.rectTransform.sizeDelta = new Vector2(400, imager.rectTransform.sizeDelta.y);
+            alive = false;
+        }
+    }
+   */
 }
